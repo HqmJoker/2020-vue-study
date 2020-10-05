cd409162e7cd5d3266173edb3448f32748d119bd
HqmJoker <44420292+HqmJoker@users.noreply.github.com>
2020-10-05 11:37:31 +0800

    简易版vue-router插件实现

---
+++ myrouter.js
@@ -0,0 +1,68 @@
+import Home from "./views/Home";
+import About from "./views/About";
+import Vue from 'vue';
+class VueRouter {
+    constructor(options) {
+        this.$options = options;
+        this.routesMap = {}
+        this.app = new Vue({
+            data: {
+                current: '/'
+            }
+        });
+    }
+
+    init(){
+        this.bindEvents();  // 监听url变化
+        this.createRoutesMap(this.$options);  // 解析路由配置
+        this.initComponents();  // 实现组件
+    }
+    bindEvents() {
+        window.addEventListener("load", this.onHashChange.bind(this))
+        window.addEventListener("hashchange", this.onHashChange.bind(this))
+    }
+    createRoutesMap(options) {
+        options.routes.forEach((item)=>{
+            this.routesMap[item.path] = item.component;
+        })
+    }
+    initComponents() {
+        Vue.component("router-link", {
+            props: { to : String },
+            render(h) {
+                return h("a", { attrs: { href: "#" + this.to } }, [
+                    this.$slots.default
+                ]);
+            }
+        });
+
+        Vue.component("router-view", {
+            render: h => {
+                const comp = this.routesMap[this.app.current]
+                return h(comp);
+            }
+        })
+    }
+    onHashChange() {
+        this.app.current = window.location.hash.slice(1) || "/";
+    }
+}
+
+VueRouter.install = function(Vue){
+    // 混入
+    Vue.mixin({
+        beforeCreate() {
+            // 初始化路由配置信息
+            if (this.$options.router) {
+                Vue.prototype.$options = this.$options.router;
+                this.$options.router.init();
+            }
+        }
+    });
+}
+
+Vue.use(VueRouter); 
+
+export default new VueRouter({
+    routes: [{ path: "/", component: Home }, { path: "/about", component: About }]
+});
